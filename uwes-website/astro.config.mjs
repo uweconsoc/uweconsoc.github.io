a580194a046f7fcf66c6e1f1fde6c719326516ca
Sargun <sargunbhatti27@gmail.com>
2026-09-07 15:40:08 -0400

    Adding social links to the club page (#2)
    - Adds astro-icons as a dependency to the project
    - Adds social links to instagram, twitter and facebook

--- uwes-website/astro.config.mjs
+++ uwes-website/astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://uweconsoc.ca',
   base: '/',
   output: 'static',
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
