36e538dd6cfe8526e01172d8681170cbffad0b4a
Sargun Singh Bhatti <sargunbhatti27@gmail.com>
2026-09-07 15:05:33 -0400

    Website Optimizations
    - Adding tailwindcss to the frontend project
    - Moving team images from public/ to src/. This is preferred by astro
    - Allowing lazyloading or preloading of images based on their
      importance. This provides performance boost
    - Centralizing the shared css in global css files.
    - Adds 'npm run check` that runs `astro check` to check for code quality
    - Adding proper browser tab icon

--- uwes-website/astro.config.mjs
+++ uwes-website/astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://uweconsociety.github.io',
+  site: 'https://uweconsoc.ca',
   base: '/',
   output: 'static',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
